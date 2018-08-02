c4ead7482ffc084ce90e0a14dfc8c3bb749a2b41
Kevin Tran <kevin.tran@leonardo.com>
2018-08-02 14:03:47 -0400

    Add UnitTest for S3Utils

--- pro02-aws-java-sdk-s3/src/main/java/org/tnmk/practicespringaws/pro02awsjavasdks3/common/aws/s3/S3Utils.java
+++ pro02-aws-java-sdk-s3/src/main/java/org/tnmk/practicespringaws/pro02awsjavasdks3/common/aws/s3/S3Utils.java
@@ -1,5 +1,7 @@
 package org.tnmk.practicespringaws.pro02awsjavasdks3.common.aws.s3;
 
+import org.springframework.util.StringUtils;
+
 public class S3Utils {
     private static final String S3_SCHEME = "s3://";
     private static final String PATH_DELIMITER = "/";
@@ -7,7 +9,7 @@ public class S3Utils {
 
 
     public static void validateS3Url(String location) {
-        if (location == null) {
+        if (StringUtils.isEmpty(location)) {
             throw new IllegalArgumentException("S3Url is null");
         }
 
@@ -16,32 +18,45 @@ public class S3Utils {
         }
     }
 
+    /**
+     * If location is "s3://someBucketName/def/xyz", return "someBucketName"
+     * @param location
+     * @return
+     */
     public static String getBucketNameFromLocation(String location) {
         validateS3Url(location);
-
-        int bucketEndIndex = location.indexOf(PATH_DELIMITER, S3_SCHEME.length());
-        if (bucketEndIndex == -1 || bucketEndIndex == S3_SCHEME.length()) {
-            throw new IllegalArgumentException("The location :'" + location + "' does not contain a valid bucket name");
-        }
+        int bucketEndIndex = validateExistBucketNameFromLocation(location);
         return location.substring(S3_SCHEME.length(), bucketEndIndex);
     }
 
-    public static String getObjectNameFromLocation(String location) {
-        validateS3Url(location);
-        
+    /**
+     * If success, return the bucketEndIndex.
+     * @param location
+     * @return
+     */
+    private static int validateExistBucketNameFromLocation(String location){
         int bucketEndIndex = location.indexOf(PATH_DELIMITER, S3_SCHEME.length());
         if (bucketEndIndex == -1 || bucketEndIndex == S3_SCHEME.length()) {
             throw new IllegalArgumentException("The location :'" + location + "' does not contain a valid bucket name");
         }
+        return bucketEndIndex;
+    }
 
+    /**
+     *
+     * @param location If location is "s3://bucketName/def/xyz", return "def/xyz"
+     * @return
+     */
+    public static String getObjectNameFromLocation(String location) {
+        validateS3Url(location);
+        
+        int bucketEndIndex = validateExistBucketNameFromLocation(location);
         if (location.contains(VERSION_DELIMITER)) {
             return getObjectNameFromLocation(location.substring(0, location.indexOf(VERSION_DELIMITER)));
         }
-
         if (location.endsWith(PATH_DELIMITER)) {
             return location.substring(++bucketEndIndex, location.length() - 1);
         }
-
         return location.substring(++bucketEndIndex, location.length());
     }
 }
