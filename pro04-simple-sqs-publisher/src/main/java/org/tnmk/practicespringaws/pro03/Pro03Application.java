a6fddb338ca20c02764063739b8b4054af9b4d74
Kevin Tran <kevin.tran@leonardo.com>
2018-09-12 14:27:08 -0400

    Clone from pro03 to pro04-simple-sqs

---
+++ pro04-simple-sqs-publisher/src/main/java/org/tnmk/practicespringaws/pro03/Pro03Application.java
@@ -0,0 +1,13 @@
+package org.tnmk.practicespringaws.pro03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Pro03Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Pro03Application.class, args);
+  }
+}
