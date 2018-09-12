1524b42f364bcf40f4b582706be84d42ab856ca8
Kevin Tran <kevin.tran@leonardo.com>
2018-09-12 17:25:13 -0400

    Add AwsSqsConfig

--- pro04-simple-sqs-publisher/README.md
+++ pro04-simple-sqs-publisher/README.md
@@ -1,3 +1,1 @@
-pro01-simple-download-s3-by-resourceloader provides a very simple way to load resource from s3.
-But when loading the resource, it doesn't provides any metadata.
-So to get both binary data and metadata in the same request, we have to write our own code by using aws-java-sdk.
+https://aws.amazon.com/blogs/developer/using-amazon-sqs-with-spring-boot-and-spring-jms/
--- pro04-simple-sqs-publisher/src/main/java/org/tnmk/practicespringaws/pro04/AwsSqsConfig.java
+++ pro04-simple-sqs-publisher/src/main/java/org/tnmk/practicespringaws/pro04/AwsSqsConfig.java
@@ -1,22 +1,67 @@
 package org.tnmk.practicespringaws.pro04;
 
-import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+import org.springframework.messaging.handler.annotation.support.PayloadArgumentResolver;
+import org.tnmk.practicespringaws.common.resourcemanagement.aws.AwsProperties;
+
+import javax.jms.Session;
+import java.util.Collections;
 
 @Configuration
 public class AwsSqsConfig {
+
     @Bean
-    @Override
-    public ConnectionFactory connectionFactory(SqsProperties sqsProperties) {
-        return createStandardSQSConnectionFactory(sqsProperties);
+    public SQSConnectionFactory sqsConnectionFactory(AwsProperties awsProperties) {
+        SQSConnectionFactory connectionFactory =
+            new SQSConnectionFactory.Builder()
+                .withRegion(Region.getRegion(Regions.fromName(awsProperties.getRegion())))
+                .withAWSCredentialsProvider(
+                    new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(
+                            awsProperties.getAccessKey(),
+                            awsProperties.getSecretKey()
+                        )
+                    )
+                )
+                .build();
+        return connectionFactory;
     }
-    
-    protected SQSConnectionFactory createStandardSQSConnectionFactory(SqsProperties sqsProperties) {
-        AmazonSQS sqsClient = createAmazonSQSClient(sqsProperties);
-        ProviderConfiguration providerConfiguration = new ProviderConfiguration();
-        sqsProperties.getNumberOfMessagesToPrefetch()
-            .ifPresent(providerConfiguration::setNumberOfMessagesToPrefetch);
-        return new SQSConnectionFactory(providerConfiguration, sqsClient);
+
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(SQSConnectionFactory sqsConnectionFactory) {
+        DefaultJmsListenerContainerFactory factory =
+            new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("3-10");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate defaultJmsTemplate(SQSConnectionFactory sqsConnectionFactory) {
+        return new JmsTemplate(sqsConnectionFactory);
+    }
+
+
+    @Bean
+    public QueueMessageHandlerFactory queueMessageHandlerFactory() {
+        QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+
+        //set strict content type match to false
+        messageConverter.setStrictContentTypeMatch(false);
+        factory.setArgumentResolvers(Collections.<HandlerMethodArgumentResolver>singletonList(new PayloadArgumentResolver(messageConverter)));
+        return factory;
     }
 }
